7264823d5b1f6696395712e0518cbbb9eefcd1f5
Clemens Schueller <schueller.cj@gmail.com>
2021-06-30 17:23:00 +0200

    Add StartPage

---
+++ src/pages/StartPage.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
+import Card from '../components/Card'
+import Header from '../components/Header'
+
+export default function StartPage() {
+  return (
+    <Wrapper>
+      <Header></Header>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section``
